ad9ea65c66fd50fafa4f6979a8a1ab377498b0a2
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-09-20 11:56:10 +0800

    add new solution

--- DS_Lab/W2/Q1.c
+++ DS_Lab/W2/Q1.c
@@ -20,3 +20,28 @@ int main() {
         free(array[i]);
     free(array);
 }
+
+/*another solution
+#include <stdio.h>
+#include <stdlib.h>
+
+void allocArray(int ***p, int m, int n) {
+    int *data = (int *)malloc(sizeof(int*) * m * n);
+    *p = (int **)malloc(sizeof(int*) * m);
+    for (int i = 0; i < m; ++i)
+        (*p)[i] = data + i * n;
+}
+
+int main() {
+    int **array;
+    allocArray(&array, 5, 10);
+    for (int j = 0; j < 5; j++)
+        for (int k = 0; k < 10; k++)
+            array[j][k] = j * 10 + k;
+    for (int j = 0; j < 5; j++)
+        for (int k = 0; k < 10; k++)
+            printf("%p ", &(array[j][k]));
+    free(array[0]);
+    free(array);
+}
+*/
