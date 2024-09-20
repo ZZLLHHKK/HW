ad4c2cd851be3a53c9d6c64d4ec966071fcd5426
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-09-20 11:56:47 +0800

    add new solution

--- DS_Lab/W2/Q2.cpp
+++ DS_Lab/W2/Q2.cpp
@@ -26,3 +26,34 @@ int main() {
         delete[] array[i];
     delete[] array;
 }
+
+/* another solution
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+template<class T>
+class Memory {
+public:
+    static T **allocArray(int m, int n) {
+        T *data = new T[m * n];
+        T **array = new T*[m];
+        for (int i = 0; i < m; ++i)
+            array[i] = data + i * n;
+        return array;
+    }
+};
+
+int main() {
+    int **array;
+    array = Memory<int>::allocArray(5, 10);
+    for (int j = 0; j < 5; j++)
+        for (int k = 0; k < 10; k++)
+            array[j][k] = j * 10 + k;
+    for (int j = 0; j < 5; j++)
+        for (int k = 0; k < 10; k++)
+            cout << array[j][k] << " ";
+    delete[] array[0];
+    delete[] array;
+}
+*/
