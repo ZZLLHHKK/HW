d7319325257f34b2eb683db3413143f8ad613bdb
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-10-04 20:24:56 +0800

    typesetting

--- DS_Lab/W4/Q1.cpp
+++ DS_Lab/W4/Q1.cpp
@@ -6,50 +6,50 @@ using namespace std;
 
 class Node {
 public:
-	Node() : next(NULL), pre(NULL) {}
-	Node(int n) : data(n), next(NULL), pre(NULL) {}
-	int getData() { 
+    Node() : next(NULL), pre(NULL) {}
+    Node(int n) : data(n), next(NULL), pre(NULL) {}
+    int getData() { 
         return data; 
     }
-	Node *getNext() { 
+    Node *getNext() { 
         return next; 
     }
-	Node *getPre() { 
+    Node *getPre() { 
         return pre; 
     }
-	void setData(int d) { 
+    void setData(int d) { 
         data = d; 
     }
-	void setNext(Node *n) { 
+    void setNext(Node *n) { 
         next = n; 
     }
-	void setPre(Node *p) { 
+    void setPre(Node *p) { 
         pre = p; 
     }
 private:
-	int data;
-	Node *next, *pre; 
+    int data;
+    Node *next, *pre; 
 };
 
 class List {
 public:
-	List() : list(NULL) {}
-	List(int n) { 
+    List() : list(NULL) {}
+    List(int n) { 
         generate(n); 
     }
-	void generate(int n) {
-		list = NULL;
-		for(int j = 0; j < n; ++j)
-			generate();
-	}
-	void generate() {
-		Node *buf = new Node(rand() % 10);
-		buf -> setNext(list);
-		if (list != NULL)
-			list -> setPre(buf);
-		list = buf;
-	}
-	void bubbleSort() {
+    void generate(int n) {
+        list = NULL;
+        for(int j = 0; j < n; ++j)
+            generate();
+    }
+    void generate() {
+        Node *buf = new Node(rand() % 10);
+        buf -> setNext(list);
+        if (list != NULL)
+            list -> setPre(buf);
+        list = buf;
+    }
+    void bubbleSort() {
         for (Node *i = list; i != NULL; i = i -> getNext()) {
             for (Node *j = list; j -> getNext() != NULL; j = j -> getNext()) {
                 if (j -> getData() > j -> getNext() -> getData())
@@ -57,7 +57,7 @@ public:
             }
         }
     }
-	void selectionSort() {
+    void selectionSort() {
         Node *s = list;
         while (s != NULL) {
             Node *tmp = s -> getNext();
@@ -68,8 +68,8 @@ public:
             }
             s = s -> getNext();
         }
-	}
-	void insertionSort() {
+    }
+    void insertionSort() {
         for (Node *i = list; i != NULL; i = i -> getNext()) {
             Node *j = i -> getPre(), *ind = i;
             while (j != NULL) {
@@ -79,38 +79,38 @@ public:
                 j = j -> getPre();
             }
         }
-	} 
-	void print() {
-		Node *cur = list;
-		while (cur != NULL) {
-			cout << cur -> getData() << " ";
-			cur = cur -> getNext();
-		}
-		cout << endl;
-	}
+    } 
+    void print() {
+        Node *cur = list;
+        while (cur != NULL) {
+            cout << cur -> getData() << " ";
+            cur = cur -> getNext();
+        }
+        cout << endl;
+    }
     void swapN(Node *x, Node *y) {
         int tmp = x -> getData();
         x -> setData(y -> getData());
         y -> setData(tmp);
     }
 private:
-	Node *list;
+    Node *list;
 };
 
 int main() {
-	srand(time(NULL));
-	List *l = new List(10);
-	l -> print();
-	l -> bubbleSort();
-	l -> print();
-	
-	l = new List(10);
-	l -> print();
-	l -> insertionSort();
-	l -> print();
-	
-	l = new List(10);
-	l -> print();
-	l -> selectionSort();
-	l -> print();
+    srand(time(NULL));
+    List *l = new List(10);
+    l -> print();
+    l -> bubbleSort();
+    l -> print();
+
+    l = new List(10);
+    l -> print();
+    l -> insertionSort();
+    l -> print();
+
+    l = new List(10);
+    l -> print();
+    l -> selectionSort();
+    l -> print();
 }
