7f5b503bbb53883695be9408398659fd2b306697
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-10-13 14:21:52 +0800

    Update Q2.cpp

--- DS_Lab/W4/Q2.cpp
+++ DS_Lab/W4/Q2.cpp
@@ -39,46 +39,91 @@ public:
     }
     void generate(int n) {
         list = NULL;
-        for(int j = 0; j < n; ++j)
+        for (int j = 0; j < n; ++j)
             generate();
     }
     void generate() {
-        Node *buf = new Node(rand() % 10);
+        Node *buf = new Node(rand() % 10 + 1);
         buf -> setNext(list);
         if (list != NULL)
             list -> setPre(buf);
         list = buf;
     }
     void bubbleSort() {
-        for (Node *i = list; i != NULL; i = i -> getNext()) {
-            for (Node *j = list; j -> getNext() != NULL; j = j -> getNext()) {
-                if (j -> getData() > j -> getNext() -> getData())
-                    swapN(j, j -> getNext());
+        Node *be4, *aft, *tmp;
+        bool sorted = false;
+        
+        while (sorted == false) {
+            sorted = true;
+            tmp = list;
+            while (tmp -> getNext() != NULL) {
+                if (tmp -> getData() > tmp -> getNext() -> getData()) {
+                    be4 = tmp -> getPre();
+                    aft = tmp -> getNext();
+                    if (be4 != NULL)
+                        be4 -> setNext(aft);
+                    else
+                        list = aft;
+                    tmp -> setNext(aft -> getNext());
+                    if (aft -> getNext() != NULL) {
+                        Node *i = aft -> getNext();
+                        i -> setPre(tmp);
+                    }
+                    tmp -> setPre(aft);
+                    aft -> setNext(tmp);
+                    aft -> setPre(be4);
+                    sorted = false;
+                }
+                else
+                    tmp = tmp -> getNext();
             }
         }
     }
     void selectionSort() {
-        Node *s = list;
-        while (s != NULL) {
-            Node *tmp = s -> getNext();
-            while (tmp != NULL) {
-                if (tmp -> getData() < s -> getData())
-                    swapN(s, tmp);
-                tmp = tmp -> getNext();
+        Node *cur = list;
+        while (cur != NULL) {
+            Node *min = cur;
+            Node *run = cur -> getNext();
+            while (run != NULL) {
+                if (run -> getData() < min -> getData())
+                    min = run;
+                run = run -> getNext();
             }
-            s = s -> getNext();
+            if (min != cur) {
+                swapN(cur, min);
+                cur = min -> getNext();
+            }
+            else 
+                cur = cur -> getNext(); 
         }
     }
     void insertionSort() {
-        for (Node *i = list; i != NULL; i = i -> getNext()) {
-            Node *j = i -> getPre(), *ind = i;
-            while (j != NULL) {
-                if (j -> getData() > ind -> getData())
-                    swapN(j, ind);
-                ind = j;
-                j = j -> getPre();
+        Node *cur = list;
+        Node *ans = NULL;
+
+        while (cur != NULL) {
+            Node *next = cur -> getNext();
+            if (ans == NULL || ans -> getData() >= cur -> getData()) {
+                cur -> setNext(ans);
+                if (ans != NULL)
+                    ans -> setPre(cur);
+                cur -> setPre(NULL);
+                ans = cur;
             }
+            else {
+                Node *tmp = ans;
+                while (tmp -> getNext() != NULL && tmp -> getNext() -> getData() < cur -> getData())
+                    tmp = tmp -> getNext();
+                cur -> setNext(tmp -> getNext());
+                cur -> setPre(tmp);
+                tmp -> setNext(cur);
+                if (cur -> getNext() != NULL)
+                    cur -> getNext() -> setPre(cur);
+            }
+            cur = next;
         }
+
+        list = ans;
     } 
     void print() {
         Node *cur = list;
@@ -89,9 +134,46 @@ public:
         cout << endl;
     }
     void swapN(Node *x, Node *y) {
-        int tmp = x -> getData();
-        x -> setData(y -> getData());
-        y -> setData(tmp);
+        if (x == y) return;
+        bool adj = (x -> getNext() == y || y -> getNext() == x);
+        if (adj) {
+            if (x -> getNext() == y) {
+                Node *preX = x -> getPre();
+                Node *nextY = y -> getNext();
+                if (preX != NULL) 
+                    preX -> setNext(y);
+                else 
+                    list = y;
+                y -> setPre(preX);
+                y -> setNext(x);
+                x -> setPre(y);
+                x -> setNext(nextY);
+                if (nextY != NULL)
+                    nextY -> setPre(x);
+            }
+            else 
+                swapN(y, x);
+        }
+        else {
+            Node *preX = x -> getPre(), *nextX = x -> getNext();
+            Node *preY = y -> getPre(), *nextY = y -> getNext();
+            if (preX != NULL)
+                preX -> setNext(y);
+            else
+                list = y;
+            if (preY != NULL)
+                preY -> setNext(x);
+            else
+                list = x;
+            if (nextX != NULL)
+                nextX -> setPre(y);
+            if (nextY != NULL)
+                nextY -> setPre(x);
+            x -> setPre(preY);
+            x -> setNext(nextY);
+            y -> setPre(preX);
+            y -> setNext(nextX);
+        }
     }
 private:
     Node *list;
