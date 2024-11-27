4d86dc24b9bfe7eb80125412863c7022c7496b6a
ZZLLHHKK <157588042+ZZLLHHKK@users.noreply.github.com>
2024-11-27 19:57:09 +0800

    Update Q3.cpp
    layout seems weird

--- DS_Lab/W10/Q3.cpp
+++ DS_Lab/W10/Q3.cpp
@@ -12,31 +12,31 @@ using namespace std;
 template<class T>
 class Node {
 public:
-	Node() {
-		data = new T;
-	}
-	Node(T d) {
-		data = new T;
-		(*data) = d;
-	}
-	Node &operator=(T d) {
-		(*data) = d;
-		return *this;
-	}
-	friend ostream &operator<<(ostream &out, Node n) {
-		out << *(n.data);
-		return out;
-	}
-	friend ostream &operator<<(ostream &out, Node *n) {
-		out << *(n->data);
-		return out;
-	}
-	void setData(T d) {
-		*data = d;
-	}
-	T &getData() const {
-		return *data;
-	}
+    Node() {
+        data = new T;
+    }
+    Node(T d) {
+        data = new T;
+        (*data) = d;
+    }
+    Node &operator=(T d) {
+        (*data) = d;
+        return *this;
+    }
+    friend ostream &operator<<(ostream &out, Node n) {
+        out << *(n.data);
+        return out;
+    }
+    friend ostream &operator<<(ostream &out, Node *n) {
+        out << *(n->data);
+        return out;
+    }
+    void setData(T d) {
+        *data = d;
+    }
+    T &getData() const {
+        return *data;
+    }
 protected:
 	T *data;
 };
@@ -44,44 +44,44 @@ protected:
 template<class T>
 class BinaryTreeNode : public Node<T> {
 public:
-	BinaryTreeNode() : Node<T>() {
-		left = NULL;
-		right = NULL;
-	}
-	BinaryTreeNode(T d) : Node<T>(d) {
-		left = NULL;
-		right = NULL;
-	}
-	BinaryTreeNode(BinaryTreeNode<T> *l, BinaryTreeNode<T> *r) : Node<T>() {
-		left = l;
-		right = r;
-	}
-	BinaryTreeNode(T d, BinaryTreeNode<T> *l, BinaryTreeNode<T> *r) : Node<T>(d) {
-		left = l;
-		right = r;
-	}
-	void setLeft(BinaryTreeNode<T> *l) {
-		left = l;
-	}
-	void setRight(BinaryTreeNode<T> *r) {
-		right = r;
-	}
-	BinaryTreeNode<T> *&getLeft() {
-		return left;
-	}
-	BinaryTreeNode<T> *&getRight() {
-		return right;
-	}
-	bool operator>(BinaryTreeNode<T> n) {
-		if (*(this->data) > *(n.data))
-			return true;
-		return false;
-	}
-	bool operator==(BinaryTreeNode<T> n) {
-		if (*(this->data) == *(n.data))
-			return true;
-		return false;
-	}
+    BinaryTreeNode() : Node<T>() {
+        left = NULL;
+        right = NULL;
+    }
+    BinaryTreeNode(T d) : Node<T>(d) {
+        left = NULL;
+        right = NULL;
+    }
+    BinaryTreeNode(BinaryTreeNode<T> *l, BinaryTreeNode<T> *r) : Node<T>() {
+        left = l;
+        right = r;
+    }
+    BinaryTreeNode(T d, BinaryTreeNode<T> *l, BinaryTreeNode<T> *r) : Node<T>(d) {
+        left = l;
+        right = r;
+    }
+    void setLeft(BinaryTreeNode<T> *l) {
+        left = l;
+    }
+    void setRight(BinaryTreeNode<T> *r) {
+        right = r;
+    }
+    BinaryTreeNode<T> *&getLeft() {
+        return left;
+    }
+    BinaryTreeNode<T> *&getRight() {
+        return right;
+    }
+    bool operator>(BinaryTreeNode<T> n) {
+        if (*(this->data) > *(n.data))
+            return true;
+        return false;
+    }
+    bool operator==(BinaryTreeNode<T> n) {
+        if (*(this->data) == *(n.data))
+            return true;
+        return false;
+    }
 private:
 	BinaryTreeNode<T> *left, *right;
 };
@@ -146,11 +146,11 @@ public:
             return rotateright(n);
         if (b < -1 && d > n->getRight()->getData()) //RR case
             return rotateleft(n);
-        if (b > 1 && d > n->getLeft()->getData()) { //RL case
+        if (b > 1 && d > n->getLeft()->getData()) { //LR case
             n->setLeft(rotateleft(n->getLeft()));
             return rotateright(n);
         }
-        if (b < -1 && d < n->getRight()->getData()) { //LR case
+        if (b < -1 && d < n->getRight()->getData()) { //RL case
             n->setRight(rotateright(n->getRight()));
             return rotateleft(n);
         }
@@ -167,6 +167,6 @@ int main() {
     srand(0);
     for (int j = 0; j < 20; ++j) {
         tree->insert(rand() % 100);
-        tree->inorder();
     }
+    tree->inorder();
 }
